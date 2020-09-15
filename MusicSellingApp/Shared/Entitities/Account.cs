32b3dffc3a5b021e802cf654b3c053b775e76d1a
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-09-15 12:05:31 -0400

    Add validation in Account

--- MusicSellingApp/Shared/Entitities/Account.cs
+++ MusicSellingApp/Shared/Entitities/Account.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicSellingApp.Shared.Entitities
 {
     public class Account
     {
         public  long Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
         public string Username { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+
         public string Password { get; set; }
+
+
+        [EmailAddress]
+        public string Email { get; set; }
 
         public DateTime CreationDate { get; set; }
 
