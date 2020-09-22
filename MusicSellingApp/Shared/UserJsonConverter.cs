ca5ca60ea176f25502490a6bcdb314ad68528e90
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-09-21 20:10:03 -0400

    Deserialize user to artist ,fan or admin

---
+++ MusicSellingApp/Shared/UserJsonConverter.cs
@@ -0,0 +1,40 @@
+﻿using MusicSellingApp.Shared.Entitities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicSellingApp.Shared
+{
+
+    public class UserJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(User));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            if (jo["FooBarBuzz"].Value<string>() == "Artist")
+                return jo.ToObject<Artist>(serializer);
+
+            if (jo["FooBarBuzz"].Value<string>() == "Fan")
+                return jo.ToObject<Fan>(serializer);
+
+            return null;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
