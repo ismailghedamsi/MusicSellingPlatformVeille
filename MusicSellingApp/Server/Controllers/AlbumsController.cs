8b41d765467e6a81d0b26f8dbe712b1993b31597
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-09-21 20:12:01 -0400

    Add a controller to apply crud operations on albums

---
+++ MusicSellingApp/Server/Controllers/AlbumsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicSellingApp.Server;
+using MusicSellingApp.Shared.Entitities;
+
+namespace MusicSellingApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlbumsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Albums
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Album>>> GetAlbums()
+        {
+            return await _context.Albums.ToListAsync();
+        }
+
+        [HttpGet("{artistId}")]
+        public async Task<ActionResult<List<Album>>> GetAlbumsOfArtist(long artistId)
+        {
+            List<Album> albums = await _context.Albums.Where(album => album.ArtistId == artistId).ToListAsync();
+            return albums;
+        }
+
+  
+
+        // PUT: api/Albums/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlbum(long id, Album album)
+        {
+            if (id != album.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(album).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlbumExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Albums
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Album>> PostAlbum(Album album)
+        {
+            _context.Albums.Add(album);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAlbum", new { id = album.Id }, album);
+        }
+
+        // DELETE: api/Albums/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Album>> DeleteAlbum(long id)
+        {
+            var album = await _context.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            _context.Albums.Remove(album);
+            await _context.SaveChangesAsync();
+
+            return album;
+        }
+
+        private bool AlbumExists(long id)
+        {
+            return _context.Albums.Any(e => e.Id == id);
+        }
+    }
+}
