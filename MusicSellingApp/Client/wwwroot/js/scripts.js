3e9f92fd72a6b51d33c122990a5ccf4ceef81786
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2020-10-10 17:58:31 -0400

    Simplify bank transaction to buy the album

---
+++ MusicSellingApp/Client/wwwroot/js/scripts.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51HakXCLFPzNgSBSANeSzkeuB3Auq48VYiEnswBQRz957pA8rhSnfFnrqVAPQMqI1P8nIh0vkz2KgWHSLuL2Vx2Kn00S38a7TJd');
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+};
